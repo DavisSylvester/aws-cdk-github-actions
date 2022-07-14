28a8506c67188917b5c56016184604ad79bda62b
Davis Sylvester <davis@sylvesterllc.com>
2022-07-14 10:24:34 -0500

    Initial commit

---
+++ test/lambda-github-actions.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaGithubActions from '../lib/lambda-github-actions-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-github-actions-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaGithubActions.LambdaGithubActionsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
