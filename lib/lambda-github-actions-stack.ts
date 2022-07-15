86a03f91f721d530ca0654107a9ef643a9efee59
Sylvester <dsylv@88665a435b45.ant.amazon.com>
2022-07-15 09:11:26 -0500

    Updating: git push

--- lib/lambda-github-actions-stack.ts
+++ lib/lambda-github-actions-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { Bucket, BucketProps } from 'aws-cdk-lib/aws-s3';
+import { Bucket, BucketProps, HttpMethods } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, BucketDeploymentProps, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 import { stackConfig } from '../config/stackConfig';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -13,6 +14,31 @@ export class LambdaGithubActionsStack extends Stack {
       bucketName: stackConfig.environments.development.resources.buckets[0].name
     };
     
-    new Bucket(this, stackConfig.environments.development.resources.buckets[0].name, bucketProps)
+    new Bucket(this, stackConfig.environments.development.resources.buckets[0].name, bucketProps);
+
+
+    const bucketAppProps: BucketProps = {
+      bucketName: 'angular-app-one',
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: '404.html',
+      publicReadAccess: true,
+      cors: [
+        {
+          allowedMethods: [HttpMethods.GET, HttpMethods.POST, HttpMethods.PUT, HttpMethods.DELETE, HttpMethods.HEAD],
+          allowedOrigins: ["*"],
+          allowedHeaders: ["*"]
+        }
+      ]
+
+    };
+    
+    const angularAppBucket = new Bucket(this, 'web-deploy', bucketProps);
+
+    
+    const bucketDeploymentProps: BucketDeploymentProps = {
+      sources: [Source.asset("/Users/dsylv/projects/samples/angular-demo/dist/angular-demo")],
+      destinationBucket: angularAppBucket
+    };
+    new BucketDeployment(this, 'angular-app-deploy', bucketDeploymentProps)
   }
 }
